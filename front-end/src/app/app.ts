6b00dbcba124b066c42737ac82e190a5c96c3f06
Arianit <arianit.selimi@outlook.com>
2025-12-19 19:56:51 +0100

    Default initialization of dashboard & add log out functionality to redirect to login page

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true, 
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-end');
+
+   private readonly router = inject(Router);
+
+  logout(): void {
+    localStorage.removeItem('token'); 
+    
+    this.router.navigate(['/login']);
+  }
 }
